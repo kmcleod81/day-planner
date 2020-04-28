069ef6fec365be0b723c7bb11c4099bba31f8b3f
kdawg <mcleod.karla@gmail.com>
2020-04-27 20:41:49 -0400

    added psuedocode inside javascript file

---
+++ assets/javascript/script.js
@@ -0,0 +1,7 @@
+// when the planner page is opened, the current day is displayed on the top inside the .time-block
+
+// when you scroll down, the time blocks have standard business hours 9-5.
+    // time blocks are color coded to indicate past, present & future
+    // when you click inside a <textarea> you can enter an event
+    // when you click the .saveBtn the text is saved into local storage
+    // when you refresh the page, the saved event stays on the page
